7ff62f6e673c7f1b42ac93f8977e78dc791dc777
Jetlove19 <rbrorlove@gmail.com>
2023-03-09 12:01:00 +0300

    Добавьте файлы проекта.

---
+++ 10.5.Practicum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _10._5.Practicum
+{
+    class Program
+    {
+        public class Calculation : ISum
+        {
+            public void Calculate()
+            {
+                double firstValue, secondValue;
+
+                while (true)
+
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите число 1");
+                        firstValue = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Введите число 2");
+                        secondValue = double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception )
+                    {
+
+                        Console.WriteLine("Ошибка!Нужно ввести число"); 
+                        continue;
+                    }
+                    double result = firstValue + secondValue;
+                        Console.WriteLine($"Сумма чисел = {result}");
+                        Console.WriteLine();     
+                }
+            }   
+        }
+        public interface ISum
+        {
+            public void Calculate();
+        }
+        static void Main(string[] args)
+        {
+
+            Calculation calculation = new Calculation();
+            ISum sum = new Calculation();
+            sum.Calculate();
+        }
+    }
+}
